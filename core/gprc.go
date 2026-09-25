037efba7be5da268e208e2076f8b1c9fadfec959
程小成 <54830642+RenXinIT@users.noreply.github.com>
2026-09-25 08:25:47 +0800

    Change gRPC server port from 50051 to 50052

--- core/gprc.go
+++ core/gprc.go
@@ -11,7 +11,7 @@ import (
 // protoc --go_out=. -I. --go-grpc_out=.  bucket.proto
 func init() {
 	go func() {
-		lis, err := net.Listen("tcp", ":50051")
+		lis, err := net.Listen("tcp", ":50052")
 		if err != nil {
 			log.Fatalf("failed to listen: %v", err)
 		}
